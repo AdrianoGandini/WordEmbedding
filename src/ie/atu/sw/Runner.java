526b9611e260fbb233a8a026c0c8b16eb2694c5c
Adriano Gandini <adrianogandini30@gmail.com>
2024-06-26 11:49:42 +0100

    Complete Runner class, initializing the dependencies and create an instance of Menu call.

--- src/ie/atu/sw/Runner.java
+++ src/ie/atu/sw/Runner.java
@@ -1,8 +1,22 @@
 package ie.atu.sw;
 
+import java.util.Scanner;
+
 public class Runner {
 
 	public static void main(String[] args) {
-
+		
+		//Initializing dependencies
+		Configuration config = new Configuration();
+		FileReaderUtility file = new FileReaderUtility();
+		Sort sort = new Sort();
+		Scanner s = new Scanner(System.in);
+		EmbeddingAnalyzer analyzer = new EmbeddingAnalyzer(file, sort);
+		WordEmbeddingIO io = new WordEmbeddingIO(config, analyzer, file);
+		
+		//Create a menu instance and pass all dependencies
+		Menu menu = new Menu(io, config, file, analyzer, sort, s);
+		
+		menu.start();
 	}
 }
